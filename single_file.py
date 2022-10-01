1f7ddb4d7e0ef22e90ba20a7350d11284a7bac30
Edgesenseconnect <113833572+edgesenseconnect@users.noreply.github.com>
2022-10-02 00:18:46 +0530

    Create single_file.py

---
+++ single_file.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import contextlib as __stickytape_contextlib
+
+@__stickytape_contextlib.contextmanager
+def __stickytape_temporary_dir():
+    import tempfile
+    import shutil
+    dir_path = tempfile.mkdtemp()
+    try:
+        yield dir_path
+    finally:
+        shutil.rmtree(dir_path)
+
+with __stickytape_temporary_dir() as __stickytape_working_dir:
+    def __stickytape_write_module(path, contents):
+        import os, os.path
+
+        def make_package(path):
+            parts = path.split("/")
+            partial_path = __stickytape_working_dir
+            for part in parts:
+                partial_path = os.path.join(partial_path, part)
+                if not os.path.exists(partial_path):
+                    os.mkdir(partial_path)
+                    with open(os.path.join(partial_path, "__init__.py"), "wb") as f:
+                        f.write(b"\n")
+
+        make_package(os.path.dirname(path))
+
+        full_path = os.path.join(__stickytape_working_dir, path)
+        with open(full_path, "wb") as module_file:
+            module_file.write(contents)
+
+    import sys as __stickytape_sys
+    __stickytape_sys.path.insert(0, __stickytape_working_dir)
+
+    __stickytape_write_module('folder1/json_reader.py', b'import json\n\nwith open(r"C:\\Users\\THISPC\\Desktop\\Package\\folder1\\config.json","r")as fb:\n    data = json.loads(fb.read())\n\nip_address = data["ip"]\nport = data["port"]\nusr_name = data["usr"]\n\n\n')
+    from folder1.json_reader import *
+    print("ip address ", ip_address)
+    print("port",port)
+    print("username",usr_name)
+    
